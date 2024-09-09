1ef238a7ada838498be89f7fa014321a33a1f5b1
Nfjdbfhr <114594474+Nfjdbfhr@users.noreply.github.com>
2024-09-09 18:23:40 -0400

    Update Room.cs

--- Room.cs
+++ Room.cs
@@ -14,7 +14,7 @@ public class Room
 
     public int[,] roomSpace;
 
-    public List<Room> neigborRooms = new List<Room>();
+    public List<Room> neighborRooms = new List<Room>();
 
     public Room aboveNeighbor;
     public Room belowNeighbor;
@@ -24,17 +24,19 @@ public class Room
     public bool isEmpty = false;
     public bool isStartRoom = false;
 
+    // Constructor to initialize the room with maximum and minimum sizes
     public Room(int maxSize, int minSize)
     {
         maxRoomDimensions = maxSize;
         minRoomDimensions = minSize;
 
-        roomSpace = new int [maxRoomDimensions, maxRoomDimensions];
+        roomSpace = new int[maxRoomDimensions, maxRoomDimensions];
     }
 
+    // Generate the layout of the room
     public void GenerateRoom()
     {
-        roomSpace = new int [maxRoomDimensions, maxRoomDimensions];
+        roomSpace = new int[maxRoomDimensions, maxRoomDimensions];
 
         int roomLength = Random.Range(minRoomDimensions, maxRoomDimensions + 1);
         int roomHeight = Random.Range(minRoomDimensions, maxRoomDimensions + 1);
@@ -46,18 +48,19 @@ public class Room
         {
             for (int j = colsOver; j < colsOver + roomLength; j++)
             {
-                roomSpace[i, j] = 1;
+                roomSpace[i, j] = 1; // Mark room space
             }
         }
 
-        FindRoomCenter(rowsDown, colsOver, roomLength, roomHeight);
+        FindRoomCenter(rowsDown, colsOver, roomLength, roomHeight); // Find and mark the center of the room
     }
 
+    // Find the center position of the room
     public void FindRoomCenter(int rowsDown, int colsOver, int roomLength, int roomHeight)
     {
         centerX = rowsDown + (roomHeight / 2);
         centerY = colsOver + (roomLength / 2);
 
-        roomSpace[centerX, centerY] = 2;
+        roomSpace[centerX, centerY] = 2; // Mark the center of the room
     }
 }
