a8350e3d77e97be6d68a36a1ab59072fd1dea8ed
Nfjdbfhr <114594474+Nfjdbfhr@users.noreply.github.com>
2024-09-09 18:23:21 -0400

    Update DungeonGenerator.cs

--- DungeonGenerator.cs
+++ DungeonGenerator.cs
@@ -5,7 +5,7 @@ using System.Linq;
 
 public class DungeonGenerator : MonoBehaviour
 {
-
+    // Parameters for dungeon generation
     public int maxNumOfRooms = 9;
     public int minNumOfRooms = 4;
     public int numOfRows = 3;
@@ -13,49 +13,55 @@ public class DungeonGenerator : MonoBehaviour
     public int maxRoomSize = 25;
     public int minRoomSize = 8;
 
+    // Prefabs for different types of tiles
     public GameObject floorTile;
     public GameObject centerTile;
     public GameObject startTile;
     public GameObject endTile;
 
+    // Lists to keep track of rooms and instantiated tiles
     public List<Room> rooms = new List<Room>();
     public List<GameObject> floorObjects = new List<GameObject>();
 
     // Start is called before the first frame update
     void Start()
     {
-        StartGenerating();
+        StartGenerating(); // Begin dungeon generation
     }
 
-    // Update is called once per frame
+    // Update is called once per frame (not used in this script)
     void Update()
     {
         
     }
 
+    // Main method to start generating the dungeon
     public void StartGenerating()
     {
+        // Validate grid parameters
         if (maxNumOfRooms != numOfRows * numOfCols)
         {
             Debug.LogError("Incorrect grid parameters: maxNumOfRooms must equal numOfRows * numOfCols");
             return;
         }
 
-        ClearPreviousGeneration();
-        GenerateFloor();
+        ClearPreviousGeneration(); // Clear previous dungeon
+        GenerateFloor(); // Generate the new dungeon floor
     }
 
+    // Clear any previously generated dungeon tiles
     public void ClearPreviousGeneration()
     {
         foreach (GameObject tile in floorObjects)
         {
-            Destroy(tile);
+            Destroy(tile); // Remove each tile from the scene
         }
 
-        floorObjects.Clear();
-        rooms.Clear();
+        floorObjects.Clear(); // Clear the list of floor objects
+        rooms.Clear(); // Clear the list of rooms
     }
 
+    // Generate the dungeon floor by creating rooms and placing them
     public void GenerateFloor()
     {   
         int skipsLeft = maxNumOfRooms - minNumOfRooms;
@@ -66,33 +72,35 @@ public class DungeonGenerator : MonoBehaviour
             {
                 skipsLeft--;
                 rooms.Add(new Room(maxRoomSize, minRoomSize));
-                rooms[i].isEmpty = true;
+                rooms[i].isEmpty = true; // Mark room as empty
                 continue;
             }
 
             rooms.Add(new Room(maxRoomSize, minRoomSize));
-            rooms[i].GenerateRoom();
+            rooms[i].GenerateRoom(); // Generate the room layout
         }
 
-        ChooseStartAndEndRoom();
+        ChooseStartAndEndRoom(); // Assign start and end rooms
     }
 
+    // Randomly select start and end rooms
     public void ChooseStartAndEndRoom()
     {
         List<Room> nonEmptyRooms = rooms.Where(room => !room.isEmpty).ToList();
         
         int startRoomIndex = Random.Range(0, nonEmptyRooms.Count);
         nonEmptyRooms[startRoomIndex].isStartRoom = true;
-        nonEmptyRooms[startRoomIndex].roomSpace[nonEmptyRooms[startRoomIndex].centerX, nonEmptyRooms[startRoomIndex].centerY] = 3;
+        nonEmptyRooms[startRoomIndex].roomSpace[nonEmptyRooms[startRoomIndex].centerX, nonEmptyRooms[startRoomIndex].centerY] = 3; // Start room identifier
 
         nonEmptyRooms.RemoveAt(startRoomIndex);
 
         int endRoomIndex = Random.Range(0, nonEmptyRooms.Count);
-        nonEmptyRooms[endRoomIndex].roomSpace[nonEmptyRooms[endRoomIndex].centerX, nonEmptyRooms[endRoomIndex].centerY] = 4;
+        nonEmptyRooms[endRoomIndex].roomSpace[nonEmptyRooms[endRoomIndex].centerX, nonEmptyRooms[endRoomIndex].centerY] = 4; // End room identifier
 
-        assignNeighborRooms();
+        assignNeighborRooms(); // Assign neighbors to rooms
     }
 
+    // Assign neighboring rooms to each room
     public void assignNeighborRooms()
     {
         for (int i = 0; i < rooms.Count; i++)
@@ -102,39 +110,28 @@ public class DungeonGenerator : MonoBehaviour
                 continue;
             }
 
-            if (i - 1 >= 0)
+            if (i - 1 >= 0 && !rooms[i - 1].isEmpty)
             {
-                if (!rooms[i - 1].isEmpty)
-                {
-                    rooms[i].leftNeighbor = rooms[i - 1];
-                }
+                rooms[i].leftNeighbor = rooms[i - 1];
             }
-            if (i + 1 < rooms.Count)
+            if (i + 1 < rooms.Count && !rooms[i + 1].isEmpty)
             {
-                if (!rooms[i + 1].isEmpty)
-                {
-                    rooms[i].rightNeighbor = rooms[i + 1];
-                }
+                rooms[i].rightNeighbor = rooms[i + 1];
             }
-            if (i - 3 >= 0)
+            if (i - 3 >= 0 && !rooms[i - 3].isEmpty)
             {
-                if (!rooms[i - 3].isEmpty)
-                {
-                    rooms[i].aboveNeighbor = rooms[i - 3];
-                }
+                rooms[i].aboveNeighbor = rooms[i - 3];
             }
-            if (i + 3 < rooms.Count)
+            if (i + 3 < rooms.Count && !rooms[i + 3].isEmpty)
             {
-                if (!rooms[i + 3].isEmpty)
-                {
-                    rooms[i].belowNeighbor = rooms[i + 3];
-                }
+                rooms[i].belowNeighbor = rooms[i + 3];
             }
         }
 
-        DrawDungeon(CombineRoomArrays());
+        DrawDungeon(CombineRoomArrays()); // Draw the dungeon based on the combined room arrays
     }
 
+    // Combine room arrays into a single dungeon floor array
     public int[,] CombineRoomArrays()
     {
         int dungeonWidth = numOfCols * maxRoomSize;
@@ -148,7 +145,7 @@ public class DungeonGenerator : MonoBehaviour
                 for (int col = 0; col < numOfCols; col++)
                 {
                     int roomIndex = row * numOfCols + col;
-                    PlaceRoomInDungeon(floor, roomIndex, row, col);
+                    PlaceRoomInDungeon(floor, roomIndex, row, col); // Place the room in the dungeon
                 }
             }
             else 
@@ -156,7 +153,7 @@ public class DungeonGenerator : MonoBehaviour
                 for (int col = numOfCols - 1; col >= 0; col--)
                 {
                     int roomIndex = row * numOfCols + (numOfCols - 1 - col);
-                    PlaceRoomInDungeon(floor, roomIndex, row, col);
+                    PlaceRoomInDungeon(floor, roomIndex, row, col); // Place the room in the dungeon
                 }
             }
         }
@@ -164,12 +161,11 @@ public class DungeonGenerator : MonoBehaviour
         return floor;
     }
 
-
+    // Place a room's tiles in the dungeon array
     private void PlaceRoomInDungeon(int[,] floor, int roomIndex, int row, int col)
     {
         Room room = rooms[roomIndex];
 
-        
         for (int roomRow = 0; roomRow < maxRoomSize; roomRow++)
         {
             for (int roomCol = 0; roomCol < maxRoomSize; roomCol++)
@@ -181,28 +177,34 @@ public class DungeonGenerator : MonoBehaviour
         }
     }
 
-
+    // Instantiate and place the tiles in the game world
     public void DrawDungeon(int[,] floor)
     {
         for (int row = 0; row < floor.GetLength(0); row++)
         {
             for (int col = 0; col < floor.GetLength(1); col++)
             {
-                if (floor[row, col] == 1)
+                GameObject tilePrefab = null;
+
+                switch (floor[row, col])
                 {
-                    floorObjects.Add(Instantiate(floorTile, new Vector3(row, 0, col), Quaternion.identity));
+                    case 1:
+                        tilePrefab = floorTile;
+                        break;
+                    case 2:
+                        tilePrefab = centerTile;
+                        break;
+                    case 3:
+                        tilePrefab = startTile;
+                        break;
+                    case 4:
+                        tilePrefab = endTile;
+                        break;
                 }
-                else if (floor[row, col] == 2)
+
+                if (tilePrefab != null)
                 {
-                    floorObjects.Add(Instantiate(centerTile, new Vector3(row, 0, col), Quaternion.identity));
-                }
-                else if (floor[row, col] == 3)
-                {
-                    floorObjects.Add(Instantiate(startTile, new Vector3(row, 0, col), Quaternion.identity));
-                }
-                else if (floor[row, col] == 4)
-                {
-                    floorObjects.Add(Instantiate(endTile, new Vector3(row, 0, col), Quaternion.identity));
+                    floorObjects.Add(Instantiate(tilePrefab, new Vector3(row, 0, col), Quaternion.identity));
                 }
             }
         }
